94eb48eeadec0575bf2668309ac031eff9e40981
TzeMingHo <tzemingho@gmail.com>
2025-10-30 17:44:23 +0000

    render first episode

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function createEpisodeCard() {}
+
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const root = document.getElementById("root");
+  const card = document.getElementById("episodeCard").content.cloneNode(true);
+  card.getElementById("episodeName").textContent = episodeList[0].name;
+  card.getElementById("seasonEpisode").textContent = `S${String(
+    episodeList[0].season
+  ).padStart(2, 0)}E${(String(episodeList[0].number).padStart2, 0)}`;
+  card.getElementById("episodeImage").src = episodeList[0].image.medium;
+  card.getElementById("episodeSummary").innerHTML = episodeList[0].summary;
+  root.append(card);
 }
 
 window.onload = setup;
