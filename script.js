148cd5d12e3603c24aa9bb6507247148c718627c
TzeMingHo <tzemingho@gmail.com>
2025-11-05 12:36:32 +0000

    refactored the function in script

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  const state = {
+    episodes: [],
+    searchTerm: "",
+  };
+  return {
+    fetchAllEpisodes() {
+      state.episodes = getAllEpisodes();
+    },
+    createEpisodeCard({ name, season, number, url, image, summary }) {
+      const card = document
+        .getElementById("episodeCard")
+        .content.cloneNode(true);
+      card.getElementById("episodeName").textContent = name;
+      card.getElementById("seasonEpisode").textContent = `S${String(
+        season
+      ).padStart(2, 0)}E${String(number).padStart(2, 0)}`;
+      card.getElementById("episodeLink").href = url;
+      card.getElementById("episodeImage").src = image.medium;
+      card.getElementById("episodeSummary").innerHTML = summary;
+      return card;
+    },
+    render() {
+      this.fetchAllEpisodes();
+      const root = document.getElementById("root");
+      const episodeCards = state.episodes.map((episode) =>
+        this.createEpisodeCard(episode)
+      );
+      root.append(...episodeCards);
+    },
+  };
 }
 
-function createEpisodeCard({ name, season, number, url, image, summary }) {
-  const card = document.getElementById("episodeCard").content.cloneNode(true);
-  card.getElementById("episodeName").textContent = name;
-  card.getElementById("seasonEpisode").textContent = `S${String(
-    season
-  ).padStart(2, 0)}E${String(number).padStart(2, 0)}`;
-  card.getElementById("episodeLink").href = url;
-  card.getElementById("episodeImage").src = image.medium;
-  card.getElementById("episodeSummary").innerHTML = summary;
-  return card;
-}
-
-function makePageForEpisodes(episodeList) {
-  const root = document.getElementById("root");
-  const episodeCards = episodeList.map((episode) => createEpisodeCard(episode));
-  root.append(...episodeCards);
-}
-
-window.onload = setup;
+window.onload = setup().render();
